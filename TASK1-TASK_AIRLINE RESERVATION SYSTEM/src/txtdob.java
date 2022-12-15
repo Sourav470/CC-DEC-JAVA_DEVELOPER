8d01ebc3d8fa95b8d9971a3f1bef240b24f44ee8
Sourav470 <120035811+Sourav470@users.noreply.github.com>
2022-12-15 20:03:59 +0530

    Add files via upload

---
+++ TASK1-TASK_AIRLINE RESERVATION SYSTEM/src/txtdob.java
@@ -0,0 +1,23 @@
+
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sourav
+ */
+class txtdob {
+
+    static Object getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setDate(Date date1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
